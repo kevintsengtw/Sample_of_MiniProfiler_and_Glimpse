dd5ec6fac66f6313c8c54459ac503205240778ea
Kevin Tseng <kevintsengtw@gmail.com>
2014-06-19 13:25:27 +0800

    ASP.NET WebForms - ADO.NET - Glimpse
    ASP.NET WebForms - ADO.NET - Glimpse

---
+++ ASP.NET WebForms - ADO.NET - Glimpse/WebApplication1/Categories/List.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="List.aspx.cs" Inherits="WebApplication1.Categories.List" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    
+	<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+		OnRowDataBound="GridView1_RowDataBound">
+		<Columns>
+			<asp:BoundField DataField="CategoryID" HeaderText="CategoryID" />
+			<asp:BoundField DataField="CategoryName" HeaderText="CategoryName" />
+			<asp:BoundField DataField="Description" HeaderText="Description" />
+			<asp:TemplateField HeaderText="Option">
+				<ItemTemplate>
+					<asp:HyperLink ID="HyperLink_Details" runat="server" Text="Details"></asp:HyperLink>
+				</ItemTemplate>
+			</asp:TemplateField>
+		</Columns>
+	</asp:GridView>
+
+</asp:Content>
---
+++ ASP.NET WebForms - ADO.NET - Glimpse/WebApplication1/Models/Repository/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using Glimpse.Ado.AlternateType;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace WebApplication1.Models.Repository
+{
+	public abstract class BaseRepository
+	{
+		private string _connectionString;
+		public string ConnectionString
+		{
+			get { return _connectionString; }
+			set { _connectionString = value; }
+		}
+
+		public string ProviderName
+		{
+			get
+			{
+				return WebConfigurationManager.ConnectionStrings["Northwind"].ProviderName;
+			}
+		}
+
+		public BaseRepository()
+		{
+			this.ConnectionString = WebConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+		}
+
+		public BaseRepository(string connectionString)
+		{
+			if (!string.IsNullOrWhiteSpace(connectionString))
+			{
+				this._connectionString = connectionString;
+			}
+		}
+
+		public GlimpseDbConnection CreateGlimpseDbConnection()
+		{
+			return new GlimpseDbConnection(new SqlConnection(this.ConnectionString));
+		}
+	}
+}
